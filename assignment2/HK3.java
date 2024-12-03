cc86422e34b8870dcfebd2b1d0e147fb44fe0a43
owaish3301 <owaish3301@gmail.com>
2024-12-04 01:56:51 +0530

    added homework assignment keyboard input question 3

--- assignment2/HK3.java
+++ assignment2/HK3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TriangleArea {
+class TriangleArea {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter three points for a triangle: ");
