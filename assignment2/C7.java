bd3eae6465cead93ac3d27e76b627feb8a541ce5
owaish3301 <owaish3301@gmail.com>
2024-12-11 09:34:29 +0530

    added 7th question of command line

---
+++ assignment2/C7.java
@@ -0,0 +1,10 @@
+class ProductCheck {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        
+        boolean result = (a <= b * c) || (b <= a * c) || (c <= a * b);
+        System.out.println(result);
+    }
+}
